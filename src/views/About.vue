42e52485e1d02a11eba2220fa62dd23659853d33
lijiayin <390913080@qq.com>
2020-04-24 16:14:22 +0800

    commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" id = "about">
+    <h5>我的页面</h5>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" >
+    <div id = "top">
+       <h4>多加网</h4>
+     <div id = "search">
+        <el-input placeholder="热门搜索" id = "item"></el-input>
+     </div>
+
+     
+     <ul id = "nav">
+       <li v-for = "(item,index) in list" :key = "index">
+         <div></div>
+         <p>{{item}}</p>
+       </li>
+     </ul>
+
+     <ul id = "nav">
+       <li v-for = "(item,index) in list" :key = "index">
+         <div></div>
+         <p>{{item}}</p>
+       </li>
+     </ul>
+    </div>
+    
+
+      <!-- 轮播 -->
+      <div class="block">
+        <span class="demonstration">默认 Hover 指示器触发</span>
+        <el-carousel height="150px">
+          <el-carousel-item v-for="item in 4" :key="item">
+            <h3 class="small">{{ item }}</h3>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+ 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      list:['成单奖励','成单奖励','成单奖励','成单奖励']
+    }
   }
 }
 </script>
